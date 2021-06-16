24ff1f4050f01e77170f1bf0a80b8556a1f01381
TucanRase <tommy.karlsen@hotmail.es>
2021-06-16 18:42:47 +0100

    Footer and finishg the project

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,13 +8,13 @@ function Cards() {
 			<div className='cards__container'>
 				<div className='cards__wrapper'>
 					<ul className='cards__items'>
-						<CardItem src='images/norge.jpg' text='Explore the hidden waterfall deep inside the Amazon Jungle' label='Adventure' path='/services' />
-						<CardItem src='images/nublo.jpg' text='Vaya al lugar de rodaje de la famosa serie de Netflix The Witcher' label='Tourism' path='/services' />
+						<CardItem src={require("../images/norge.jpg").default} text='Explore the hidden waterfall deep inside the Amazon Jungle' label='Adventure' path='/services' />
+						<CardItem src={require("../images/nublo.jpg").default} text='Vaya al lugar de rodaje de la famosa serie de Netflix The Witcher' label='Tourism' path='/services' />
 					</ul>
 					<ul className='cards__items'>
-						<CardItem src='images/awita.jpg' text='Explore the beautiful fjord of norway with us, in the Preikestolen' label='Adventure' path='/services' />
-						<CardItem src='images/casa.jpg' text='Come with and take a look at these cozy houses by the sea' label='Tourism' path='/services' />
-						<CardItem src='images/piedra.jpg' text="Let's go hiking around norway this places will take your breath away" label='Hiking' path='/services' />
+						<CardItem src={require("../images/piedra.jpg").default} text='Explore the beautiful fjord of norway with us, in the Preikestolen' label='Adventure' path='/services' />
+						<CardItem src={require("../images/casa.jpg").default} text='Come with and take a look at these cozy houses by the sea' label='Tourism' path='/services' />
+						<CardItem src={require("../images/awita.jpg").default} text="Let's go hiking around norway this places will take your breath away" label='Hiking' path='/services' />
 					</ul>
 				</div>
 			</div>
